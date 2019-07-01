b667200d11b68b57bf536ffcdc5226fa721dbce0
peter <perter@126.com>
2019-07-01 18:50:09 +0800

    增强 StringHelper 功能

--- tests/infrastructure/helper/TestEntity.php
+++ tests/infrastructure/helper/TestEntity.php
@@ -3,7 +3,7 @@
  * 注意：本内容仅限于博也公司内部传阅,禁止外泄以及用于其他的商业目的
  * @author    hebidu<346551990@qq.com>
  * @copyright 2017 www.itboye.com Boye Inc. All rights reserved.
- * @link      http://www.itboye.com/
+ *
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  * Revision History Version
  ********1.0.0********************
